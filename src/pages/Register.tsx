2ee9a3c2352a4139aa86c4b9e1383f0613fbb687
deepbhut22 <deeppatel6071@gmail.com>
2025-05-07 16:23:35 +0530

    vercel.json added

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -22,7 +22,7 @@ const Register = () => {
   };
 
   const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:5000/api/auth/google';
+    window.location.href = 'https://api.pathpr.ca/api/auth/google';
   };
 
   return (
